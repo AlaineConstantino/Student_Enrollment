a454111db7606f860b4070245827c1ef59cab837
Alaine Nicole Constantino <151738760+AlaineConstantino@users.noreply.github.com>
2025-12-31 20:29:14 +0800

    Fix 419 Page Expired error: Add CSRF token to axios and session keep-alive

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,16 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Set CSRF token for AJAX requests
+let token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+}
+
+// Keep session alive by making a request every 30 minutes
+setInterval(() => {
+    axios.get('/').catch(() => {
+        // Ignore errors, just to keep session alive
+    });
+}, 30 * 60 * 1000); // 30 minutes
